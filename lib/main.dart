740b3785dcccb0ad0e20ad594d6ad256b0821ae1
Kenneth <anthonyedem46@gmail.com>
2024-02-02 08:38:48 +0000

    new app for test restfull api

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: home() ,
+    );
+  }
+}
+
+class home extends StatefulWidget {
+  const home({super.key});
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        backgroundColor:Colors.green,
+      ),
+      body: Container(
+        child: ElevatedButton(onPressed: (){}, child: Text("Afficher")),
+      ),
+    );
+  }
+}
+
+
